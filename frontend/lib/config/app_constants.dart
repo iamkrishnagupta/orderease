6fa4a5c9035834b22adb854f91b79ab1b1fe5734
Krishna Gupta <krishna.ghhh@gmail.com>
2024-05-06 13:36:21 +0530

    No changes made

---
+++ frontend/lib/config/app_constants.dart
@@ -0,0 +1,12 @@
+class AppConstants {
+  static const initialTableNumber = 'Find your table?';
+  static const List<String> tableNumbers = [
+    'Find your table?',
+    'TBN_Q=TBID?A1',
+    'TBN_Q=TBID?A2',
+    'TBN_Q=TBID?A3',
+    'TBN_Q=TBID?B1',
+    'TBN_Q=TBID?B2',
+    'TBN_Q=TBID?C1',
+  ];
+}
---
+++ frontend/lib/config/constants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  static const Color primaryColor = Colors.teal;
+  static const Color accentColor = Colors.white;
+  static const double iconSize = 28.0;
+}
