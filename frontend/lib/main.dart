c2aca074a641fb6f2e9f4aaddfa5121fbafdbafe
Krishna Gupta <krishna.ghhh@gmail.com>
2024-05-09 13:13:07 +0530

    env file loaded and integrated welcome screen.

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,1 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart'; 
+import 'package:frontend/modules/welcome/welcome_screen.dart';
+import 'package:frontend/providers/cart_provider.dart';
+import 'package:frontend/providers/user_provider.dart';
+import 'package:provider/provider.dart';
 
+Future<void> main() async {
+  //lo
+  await dotenv.load(fileName: ".env");
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) => CartProvider(),
+        ),
+        ChangeNotifierProvider(
+          create: (context) => UserProvider(),
+        )
+      ],
+      child: const MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: WelcomeScreen(),
+      ),
+    );
+  }
+}
