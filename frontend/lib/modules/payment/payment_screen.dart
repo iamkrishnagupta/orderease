b0372ddac06805c06cf78d6cb34cd61d86efe21b
Krishna Gupta <krishna.ghhh@gmail.com>
2024-05-11 19:52:55 +0530

    initial commit for payment  module

---
+++ frontend/lib/modules/payment/payment_screen.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:frontend/constants/global_variables.dart';
+import 'package:frontend/modules/admin/screens/admin_home_screen.dart';
+import 'package:frontend/modules/home/screens/home_screen.dart';
+import 'package:frontend/ui_components/custom_button2.dart';
+
+class PaymentScreen extends StatefulWidget {
+  const PaymentScreen({super.key});
+
+  @override
+  State<PaymentScreen> createState() => _PaymentScreenState();
+}
+
+class _PaymentScreenState extends State<PaymentScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('PaymentScreen'),
+            backgroundColor: GlobalVariables.purple,
+
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(50.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Payment done!',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 10),
+            CustomButton2(
+                text: 'Back to Home',
+                onTap: () {
+                  Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const Home()));
+                })
+          ],
+        ),
+      ),
+    );
+  }
+}
